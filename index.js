45706ce1a2e7792ca9664df8a7f16d3fbc9923e3
Jarrett Cruger <jcrugzz@gmail.com>
2013-06-23 02:35:36 -0400

    [wip] make main module a proper client

--- index.js
+++ index.js
@@ -6,23 +6,37 @@
  */
 
 var multilevel = require('multilevel'),
+    EventEmitter = require('events').EventEmitter,
     net = require('net');
 
 var transforms = require('./lib/transforms');
 
 var LevelAgile = module.exports = function (options) {
+  if(!(this instanceof LevelAgile)) { return new LevelAgile(options) }
+
   if (!options || !options.host || !options.port || !options.transform) {
     throw new Error('A port is necessary in order to connect to the server');
   }
+  EventEmitter.call(this);
 
-  var db = multilevel.client(),
-      transform = transforms[options.transform](),
-      connectOpts = {
-        host: options.host,
-        port: options.port
-      };
+  this.db = multilevel.client();
+  this.transform = transforms[options.transform]();
+  this.connectOpts = {
+    host: options.host,
+    port: options.port
+  };
 
-  db.pipe(net.connect(connectOpts)).pipe(db);
+};
 
-  return transform.pipe(db.writeStream());
+
+LevelAgile.prototype.connect = function () {
+  //
+  // TODO: Have reconnection logic
+  //
+  this.db.pipe(net.connect(connectOpts)).pipe(this.db);
+
+};
+
+LevelAgile.prototype.writeStream = function () {
+  return this.transform.pipe(this.db.writeStream());
 };
