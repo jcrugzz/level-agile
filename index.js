5d94188bda609af79047bb8cb704e3e5c9f366ee
Jarrett Cruger <jcrugzz@gmail.com>
2013-06-23 18:21:13 -0400

    [fix] actually inherit from EventEmitter, handle transform a bit differently with writestream and add readStream

--- index.js
+++ index.js
@@ -5,21 +5,23 @@
  *
  */
 
-var multilevel = require('multilevel'),
-    EventEmitter = require('events').EventEmitter,
-    net = require('net');
+var EventEmitter = require('events').EventEmitter,
+    net = require('net'),
+    util = require('util'),
+    multilevel = require('multilevel');
+
 
 var transforms = require('./lib/transforms');
 
 var LevelAgile = function (options) {
   if (!options || !options.host || !options.port || !options.transform) {
-    throw new Error('A port is necessary in order to connect to the server');
+    throw new Error('Port and host are necessary in order to connect to the server');
   }
 
   EventEmitter.call(this);
 
   this.db = multilevel.client();
-  this.transform = transforms[options.transform]();
+  this.transform = transforms[options.transform];
   this.connectOpts = {
     host: options.host,
     port: options.port
@@ -28,6 +30,7 @@ var LevelAgile = function (options) {
   this.connect();
 };
 
+util.inherits(LevelAgile, EventEmitter);
 
 LevelAgile.prototype.connect = function () {
   //
@@ -37,8 +40,20 @@ LevelAgile.prototype.connect = function () {
 
 };
 
-LevelAgile.prototype.writeStream = function () {
-  return this.transform.pipe(this.db.writeStream());
+LevelAgile.prototype.writeStream = function (options) {
+  var ws = this.db.createWriteStream(options);
+
+  ws.on('error', this.emit.bind(this, 'error'));
+
+  return ws;
+};
+
+LevelAgile.prototype.readStream = function (options) {
+  var rs = this.db.createReadStream(options);
+
+  rs.on('error', this.emit.bind(this, 'error'));
+
+  return rs;
 };
 
 module.exports = function(options) {
