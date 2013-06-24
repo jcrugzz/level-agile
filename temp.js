b69ea98fdf6d04d26024d6f0d22b115d9b36e222
Jarrett Cruger <jcrugzz@gmail.com>
2013-06-24 02:13:24 -0400

    [dist] Version bump. 0.0.5

---
+++ temp.js
@@ -0,0 +1,16 @@
+var levelAgile = require('./');
+
+var level = levelAgile({
+  host: '127.0.0.1',
+  port: 4444,
+  transform: 'logs'
+});
+
+level.readStream()
+  .on('data', function (data) {
+    console.log(data);
+  });
+
+level.on('error', function (err) {
+  console.error(err);
+});
---
+++ temp2.js
@@ -0,0 +1,23 @@
+var levelAgile = require('./');
+var timestamp = require('monotonic-timestamp');
+
+var level = levelAgile({
+  host: '127.0.0.1',
+  port: 4444,
+  transform: 'logs'
+});
+
+var ws = level.writeStream();
+
+for (var i=0; i<200; i++) {
+  ws.write({
+    key: 'logs\x00test\x00' + timestamp(),
+    value: 'OH HAI THERE ' + i,
+    keyEncoding: 'utf8',
+    valueEncoding: 'utf8'
+  })
+}
+
+level.on('error', function (err) {
+  console.log('err');
+});
